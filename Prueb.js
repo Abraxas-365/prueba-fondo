96167c16a3e66488f94e868fc2de821e14a98dc7
Abraxas-365 <63959220+Abraxas-365@users.noreply.github.com>
2020-05-10 09:18:28 -0500

    Update Prueb.js

--- Prueb.js
+++ Prueb.js
@@ -3,6 +3,6 @@
 
 t.prefs_.set('color-palette-overrides',["#090300", "#db2d20", "#01a252", "#fded02", "#01a0e4", "#a16a94", "#b5e4f4", "#a5a2a2", "#5c5855", "#e8bbd0", "#3a3432", "#4a4543", "#807d7c", "#d6d5d4", "#cdab53", "#f7f7f7"]);
 t.prefs_.set('foreground-color', "#4a4543");
-t.prefs_.set(backgroundImage = "url('https://archive-media-1.nyafuu.org/wg/image/1427/99/1427998565618.jpg')");
+t.prefs_.set(backgroundImage = "url('https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80%27')");
 t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
 
